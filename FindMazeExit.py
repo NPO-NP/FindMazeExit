bce356226453641c690d5e3f7d368fa316d79b60
NPO-NP <78456508+NPO-NP@users.noreply.github.com>
2025-07-07 09:45:21 +0800

    Add files via upload

---
+++ FindMazeExit.py
@@ -0,0 +1,36 @@
+
+# 0 is a valid space
+# 9 is a wall, you cannot move past it
+# 1 is the exit, your goal
+# 5 is visited location
+# can only move left, right, up and down
+
+# 2 test mazes for you to verify your code
+'''
+maze = [[9,9,9,9,9,1,9],\
+        [9,9,0,9,9,0,9],\
+        [9,0,0,9,9,0,9],\
+        [9,0,9,0,0,0,9],\
+        [9,0,0,0,9,0,9],\
+        [9,9,9,9,0,0,9],\
+        [9,9,9,9,9,9,9]]
+'''
+maze = [[9,9,1,9,9,9,9],\
+        [9,9,0,9,9,0,9],\
+        [9,0,0,9,9,0,9],\
+        [9,0,9,0,0,0,9],\
+        [9,0,0,0,9,0,9],\
+        [9,9,9,9,0,0,9],\
+        [9,9,9,9,9,9,9]]
+
+NUMROWS = 7     # update these 4 constants for maze of a different size
+NUMCOLUMNS = 7  
+STARTX = 3      # starting column
+STARTY = 3      # starting row
+
+WALL = 9        # constants to represent the meaning of the values
+EXIT = 1        
+SPACE = 0       
+VISITED = 5    
+
+# Your code should display something like: Exit found at 0,2
